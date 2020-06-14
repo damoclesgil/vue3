ec201cfcb9b8155c1423c0669a770c0dca560c40
Dâmocles <damoclesgil@gmail.com>
2020-06-13 22:59:03 -0300

    Update v15 alpha

---
+++ src/components/Testing.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { defineComponent, onMounted, ref } from "vue";
+export default defineComponent({
+  name: "Testing",
+  data: () => ({
+    variavel: "123123",
+  }),
+  props: {
+    propriedade: {
+      type: String,
+      default: "12312",
+    },
+  },
+  setup(_, { attrs }) {
+    const toogle = ref<Boolean>(false);
+
+    onMounted(() => {
+      console.log(_.propriedade);
+      console.log(attrs);
+    });
+
+    return { toogle };
+  },
+});
+</script>
+
+<template>
+  <div class="elemento-purificador text-white">
+    Eu sou um elemento {{ variavel }}
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,13 @@
 <script lang="ts">
 import { defineComponent } from "vue";
-import NavRoutes from "@/components/NavRoutes";
-
+// import Testing from "@/components/Testing";
 export default defineComponent({
   name: "Home",
-  components: {
-    NavRoutes,
-  },
   setup() {
     return {};
   },
 });
 </script>
 <template>
-  <section class="mt-3 max-w-2xl m-auto">
-    <nav-routes />
-  </section>
+  <section class="mt-3 max-w-2xl m-auto"></section>
 </template>
